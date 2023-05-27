f8a0f3740854f8cd39043822075220e651f42141
malinaluc <malinaluc@yahoo.com>
2023-05-27 20:40:40 +0300

    27.05

--- a3/a3.c
+++ a3/a3.c
@@ -8,11 +8,21 @@
 #include <sys/mman.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <errno.h>
 
 int fdreq = -1;
 int fdresp = -1;
 
-// comunica pipe ului mesajul in formatul cerut
+int shmid;
+void *shmaddr;
+void *file_ptr;
+
+off_t shared_mem_size ;
+
+struct stat file_stat;
+
+int fd; ///fisierul din memoria partajata
+
 void writeStringField(char* string)
 {
     char aux[strlen(string) + 2];
@@ -25,10 +35,8 @@ void writeStringField(char* string)
 
 void writeNumberField(unsigned number)
 {
-    // scriere directa numar de tip unsigned pe 4 bytes
     write(fdresp, &number, 4);
 }
-
 
 void readStringField(char* buffer)
 {
@@ -59,23 +67,19 @@ int main()
     char *reqPipe = "REQ_PIPE_58203";
     char *respPipe = "RESP_PIPE_58203";
 
-    ///unlink(respPipe); ///la rulari consecutive, as primi eroare deoarece ar fi deja creat
-
     /// 0600 - > permisiune atat pentru scriere, cat si pentru citire <le am lasat doar pentru user>
     if(mkfifo(respPipe,0600)!= 0)
     {
         perror("ERROR\n"
-               "cannot create the response pipe\n");
+               "pipe raspuns necreat\n");
         return 1;
     }
 
     fdreq = open(reqPipe,O_RDONLY);
     if(fdreq == -1)
     {
-        perror("ERROR\n"
-               "cannot open the request pipe");
+        perror("ERROR\n"" nu se poate deschide pipe ul de request");
         return 1;
-
     }
 
     fdresp = open(respPipe,O_WRONLY);
@@ -86,13 +90,12 @@ int main()
 
     }
 
-    // comunica piep ului mesajul de start pentru a creea conexiunea
     writeStringField("HELLO");
 
     char request[100];
+
     while(1)
     {
-        // citeste continuu stringuri din pipe dupa le compara
         readStringField(request);
 
         // ex2.3
@@ -106,70 +109,134 @@ int main()
         else if (strcmp(request, "CREATE_SHM") == 0)
         {
             unsigned number = readNumberField();
-            int shmid;
-            void *shmaddr;
-
-            key_t key = ftok(".", 'R');
-            if (key == -1)
-            {
-                perror("Eroare la generarea cheii IPC");
-                exit(1);
-            }
-
-            shmid = shmget(key, number, IPC_CREAT | 664);
+            shmid = shm_open("/q7jfuq", O_CREAT| O_RDWR, 0664);
             if (shmid == -1)
             {
-                perror("Eroare la crearea regiunii de memorie partajată");
+                writeStringField("CREATE_SHM!ERROR!");
                 exit(1);
             }
 
-            off_t size = number;
-            if (ftruncate(shmid, size) == -1)
+            shared_mem_size = number;
+            if (ftruncate(shmid, shared_mem_size) == -1)
             {
-                perror("ftruncate");
+                writeStringField("CREATE_SHM!ERROR!");
                 return 1;
             }
 
-            shmaddr = shmat(shmid, NULL, 0);
+            shmaddr = mmap(NULL, shared_mem_size, PROT_READ | PROT_WRITE, MAP_SHARED,shmid, 0);
             if (shmaddr == (void*) -1)
             {
-                perror("Eroare la atașarea regiunii de memorie partajată");
+                writeStringField("CREATE_SHM!ERROR!");
                 exit(1);
             }
 
-            if (shmdt(shmaddr) == -1)
-            {
-                perror("Eroare la eliberarea regiunii de memorie partajată");
-                exit(1);
-            }
-
-
+            writeStringField("CREATE_SHM!SUCCESS!");
 
         }
         //ex2.5
         else if (strcmp(request, "WRITE_TO_SHM") == 0)
         {
+            unsigned offset = readNumberField();
+            unsigned value = readNumberField(); ///valoarea de scris in memorie la offset
+
+            if (offset <0 || offset>=3613231 || offset >= shared_mem_size + sizeof(value))
+            {
+                writeStringField("WRITE_TO_SHM!ERROR!");
+                return 1;
+            }
+
+            unsigned *ptr = (unsigned *)((char *)shmaddr + offset);
+            *ptr = value;
+            memcpy((void *)((char *)shmaddr + offset), &value, sizeof(unsigned));
+
+            writeStringField("WRITE_TO_SHM!SUCCESS!");
 
         }
         //ex2.6
         else if (strcmp(request, "MAP_FILE") == 0)
         {
 
+            char nume_fisier[100];
+            readStringField(nume_fisier);
+
+            fd = open(nume_fisier,O_RDONLY);
+            if(fd==-1)
+            {
+                writeStringField("MAP_FILE!ERROR!");
+                return 1;
+            }
+
+            if (fstat(fd, &file_stat) == -1)
+            {
+                writeStringField("MAP_FILE!ERROR!");
+                return 1;
+            }
+
+
+            file_ptr = mmap(NULL, file_stat.st_size, PROT_READ, MAP_SHARED, fd, 0);
+            if (file_ptr == MAP_FAILED)
+            {
+                writeStringField("MAP_FILE!ERROR!");
+                munmap(shmaddr, file_stat.st_size);
+                close(fd);
+                close(shmid);
+                return 1;
+            }
+
+            writeStringField("WRITE_TO_SHM!SUCCESS!");
+
         }
         //ex2.7
         else if (strcmp(request, "READ_FROM_FILE_OFFSET") == 0)
         {
+            unsigned offset = readNumberField();
+            unsigned no_of_bytes = readNumberField();
 
+            if(offset < 0 || offset+no_of_bytes>= file_stat.st_size)
+            {
+                writeStringField("READ_FROM_FILE_OFFSET!ERROR!");
+                return 1;
+            }
+
+            if(fstat(fd,&file_stat) ==-1)
+            {
+                writeStringField("READ_FROM_FILE_OFFSET!ERROR!");
+                exit(EXIT_FAILURE);
+            }
+
+            off_t file_size = file_stat.st_size;
+
+            void *file_mapping = mmap(NULL, file_size, PROT_READ,MAP_SHARED,fd,0);
+
+            if (file_mapping == (void*) -1)
+            {
+                writeStringField("READ_FROM_FILE_OFFSET!ERROR!");
+                exit(1);
+            }
+
+            char *file_data = (char*)file_mapping;
+            for(off_t i = offset ; i<no_of_bytes; i++)
+            {
+                char curr_oct = file_data[i];
+                printf("%c", curr_oct);
+            }
+
+            munmap(file_mapping, file_size);
+            close(fd);
+
+            writeStringField("WRITE_TO_SHM!SUCCESS!");
         }
         //ex2.8
         else if (strcmp(request, "READ_FROM_FILE_SECTION") == 0)
         {
 
+            writeStringField("WRITE_TO_SHM!SUCCESS!");
         }
         //ex2.9
         else if (strcmp(request, "READ_FROM_LOGICAL_SPACE_OFFSET") == 0)
         {
 
+            writeStringField("WRITE_TO_SHM!SUCCESS!");
         }
         // ex2.10 comanda de iesire
         else if (strcmp(request, "EXIT") == 0)
@@ -178,7 +245,9 @@ int main()
         }
     }
 
-
+    close(shmid);
+    close(fd);
+    munmap(shmaddr,shared_mem_size);
     close(fdreq);
     close(fdresp);
     unlink(reqPipe);
